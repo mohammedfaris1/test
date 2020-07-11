c35ff99ed46f4e6e9e72e8e072dc728db6f20078
mohammedfaris1 <mohammedfarisofficial@gmail.com>
2020-07-11 12:56:05 +0530

    Updates

---
+++ precache-manifest.204b74b28496287e8ad0a9fcb7a51120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02f0b237f2c7857568603c67a040cde",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2e01f818d697aecd90bd",
+    "url": "/react-gh-pages/static/css/main.0df41457.chunk.css"
+  },
+  {
+    "revision": "df25eb2de707043be48e",
+    "url": "/react-gh-pages/static/js/2.9ea8867e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.9ea8867e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e01f818d697aecd90bd",
+    "url": "/react-gh-pages/static/js/main.620342ae.chunk.js"
+  },
+  {
+    "revision": "298c4f1620c9abf9c6e1",
+    "url": "/react-gh-pages/static/js/runtime-main.931fd8ce.js"
+  }
+]);
